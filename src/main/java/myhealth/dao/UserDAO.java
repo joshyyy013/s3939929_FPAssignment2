0caf8733268e7f611b97e1a7e685cd64196f0569
Joshua Kumar <s3939929@student.rmit.edu.au>
2026-05-12 10:59:20 +1000

    finished userdao

--- src/main/java/myhealth/dao/UserDAO.java
+++ src/main/java/myhealth/dao/UserDAO.java
@@ -3,6 +3,62 @@ package myhealth.dao;
 import myhealth.model.User;
 import myhealth.util.DatabaseManager;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDAO {
+    private final Connection connection;
+
+    public UserDAO() {
+        connection = DatabaseManager.getInstance().getConnection();
+    }
+
+    public boolean createUser(User user) {
+        String sql = """
+                INSERT INTO users (username, password, first_name, last_name)
+                VALUES (?, ?, ?, ?)
+                """;
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getFirstName());
+            stmt.setString(4, user.getLastName());
+
+            stmt.executeUpdate();
+            return true;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public User login(String username, String password) {
+        String sql = """
+                SELECT * FROM users
+                WHERE username = ? AND password = ?
+                """;
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+
+            ResultSet result = stmt.executeQuery();
+
+            if (result.next()) {
+                return new User(
+                        result.getInt("id"),
+                        result.getString("username"),
+                        result.getString("password"),
+                        result.getString("first_name"),
+                        result.getString("last_name")
+                );
+            }
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
